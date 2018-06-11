8b4ca3d805e80cfabd37be5a3e56edf15e839d57
Tyagi <aakashtyagi1996@outlook.com>
2018-06-11 13:00:52 +0530

    Lab 1 11-06-2018 !

---
+++ Jab1/src/com/tyagi/literals/booleanliterals/BooleanLiterals.java
@@ -0,0 +1,30 @@
+package com.tyagi.literals.booleanliterals;
+
+/**
+ * @author HP
+ *
+ */
+public class BooleanLiterals {
+
+	public static void main(String arg[]) {
+		System.out.println("Program Of Boolean Literals");
+		boolean b1 = true;
+		boolean b2 = false;
+		if (b1)
+			System.out.println("If Part True");
+		else 
+			System.out.println("Else Part");
+
+		Boolean b5 = true;
+		Boolean b6 = false;
+		if (b5)
+			System.out.println("b5 is Boolean ref. Type");
+		else
+			System.out.println("Else Part");
+		
+		/*int a=10;
+		Int b=20;
+		System.out.println(a);
+		System.out.println(b);*/
+	}
+}
