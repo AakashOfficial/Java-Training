f5406b38c2869f1556e760d8f3f9258f67ea434b
Tyagi <aakashtyagi1996@outlook.com>
2018-06-11 23:55:45 +0530

    Lab 1 !

--- Jab1/src/com/tyagi/literals/booleanliterals/CharacterLiterals.java
+++ Jab1/src/com/tyagi/literals/booleanliterals/CharacterLiterals.java
@@ -22,23 +22,27 @@ public class CharacterLiterals {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		char ch1 = 'A';
-		
+		char ch1 = 'A'; // Valid
+		// Character value should not be null default is Blankspace
 		// char ch2='';//Error: Empty Char Lit.
 		//Character has Only One
 		// char ch3='AB';//
 		char ch4 = '*';
-		char ch5 = '7';
+		char ch5 = '7';// Number can be assign in Character 
 		System.out.println(ch1);
 
 		System.out.println(ch4);
 		System.out.println(ch5);
-		char ch6 = ' ';// One Space
-		char ch7 = ' ';// Tab key
-		// char ch8='';//Multiple Spaces
+		//Blank Space is Default we assign to it
+		char ch6 = ' ';// One Space 
+		
+		char ch7 = '	';// Tab key
+		// This error is due to that the default value of char is Blank Space( )
+		// char ch8='';//Multiple Spaces 
+		
 		// char ch9=''';
 		char ch10 = '\'';
-		// char ch11='\p';
+		// char ch11='\p';  // Invalid Escape Sequence Valid are \n \t \b \f \r \\ \" \'
 		char ch12 = '\u0045';
 		System.out.println(ch12);
 		char ch13 = '\u00cf';
@@ -51,13 +55,22 @@ public class CharacterLiterals {
 		char ch17 = 65535;
 		System.out.println("\n-------");
 		for (char ch = 0; ch <= 255; ch++) {
-			int x = ch;
+			int x = ch; // Assign the value to a variable of type integer 
 			System.out.print(x);
+			// System.out.print(ch);
 			
 			System.out.print('\t');// This is For Printing a Tab Space
-			System.out.println(ch);// This is For Printing the Character
+			System.out.println(ch);// This is For Printing the Character by using the ASCII Value
 			System.out.println();// This is for print Blank Line
 		}
 	}
 
 }
+
+/*
+==========================================================================================
+char ch9=''';
+
+In single quote can we write only one single quote
+*
+*/
