db17cb9b0dcaedafbe5a105b9be3fa400e185abe
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-06-19 15:21:15 +0530

    Operators !

---
+++ Practice/src/Arithmetic.java
@@ -0,0 +1,40 @@
+
+public class Arithmetic {
+
+	public static void main(String arg[]) {
+		int aa = 97;
+		System.out.println(aa);
+		int a = +aa;
+		System.out.println(a);
+		int b = -aa;
+		System.out.println(b);
+		int c = -b;
+		System.out.println(c);
+		int d = - -aa;
+		System.out.println(d);
+		int e = -(-aa);
+		System.out.println(e);
+		System.out.println(aa);
+		int f = --aa;
+		// Decrement System.out.println(f);
+		System.out.println(aa);
+		int mn = -56;
+		System.out.println(+mn);
+
+		System.out.println(-mn);
+		System.out.println(- -mn);
+		System.out.println(-(-mn));
+		System.out.println(--mn);
+		char ch = 'A';
+		char ch2 = ch;
+		// char ch1=+ch; 
+		int xy=+ch;
+		System.out.println(xy);
+		byte by1 = 46;
+		byte by2 = +46;
+		// byte by3=+by1;
+		int by4 = +by1;
+		System.out.println(by4);
+	}
+
+}
---
+++ Practice/src/RelationalOperator.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+//package com.tyagi.literals.booleanliterals;
+
+/**
+ * @author HP
+ *
+ */
+/* public */ class RelationalOperator {
+
+	/**
+	 * 
+	 */
+	public RelationalOperator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str="ABC";
+		RelationalOperator test=new RelationalOperator ();
+		// System.out.println(str==test);
+		Object obj=str;
+		System.out.println(obj==test);
+		System.out.println(obj==str);
+		obj=test;
+		System.out.println("------");
+		System.out.println(obj==test);
+		System.out.println(obj==str);
+
+	}
+
+}
